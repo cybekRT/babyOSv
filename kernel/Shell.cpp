c00ed2ab2f7685291845cec9df4444e77a887cdc
cybek <cybek@wyobraz.net>
2022-02-26 02:00:13 +0100

    Refactor shell and add macro to define functions handlers

--- kernel/Shell.cpp
+++ kernel/Shell.cpp
@@ -3,243 +3,316 @@
 #include"Thread.hpp"
 #include"FS/VFS.hpp"
 #include"Video/VGA.hpp"
+#include"Container/Array.hpp"
+#include"Container/LinkedList.hpp"
 
 namespace Shell
 {
+	FS::Directory* dir;
+
+	typedef int(*HandlerPtr)(int, char**);
+	struct HandlerDef
+	{
+		const char* name;
+		HandlerPtr handler;
+		const char* description;
+	};
+	Container::LinkedList<HandlerDef> handlers;
+
+	class RegHandler
+	{
+		public:
+			RegHandler(const char* name, HandlerPtr handler, const char* description = nullptr)
+			{
+				handlers.PushBack( { .name = name, .handler = handler, .description = description } );
+			}
+	};
+
+	#define HANDLER(name, ...) int Shell_##name(int argc, char** argv); RegHandler regHandler_##name(#name, Shell_##name __VA_OPT__(,) __VA_ARGS__); int Shell_##name(int argc, char** argv)
+
+	HANDLER(help, "Print help")
+	{
+		Print("=== Available commands ===\n");
+
+		unsigned maxLen = 0;
+		for(auto f : handlers)
+		{
+			unsigned len = strlen(f.name);
+			if(len > maxLen)
+				maxLen = len;
+		}
+
+		for(auto f : handlers)
+		{
+			Terminal::PrintWithPadding(maxLen + 2, "- %s", f.name);
+			if(f.description)
+				Print(" - %s", f.description);
+			Print("\n");
+		}
+
+		return 0;
+	}
+
+	HANDLER(mem, "Print memory map")
+	{
+		Memory::Physical::PrintMemoryMap();
+		return 0;
+	}
+
+	HANDLER(fail, "Crash system")
+	{
+		u8* x = (u8*)0x1234;
+		*x = 5;
+	}
+
+	// HANDLER(task, "Print tasks list")
+	// {
+	// 	Thread::UpdateThreadsList();
+	// 	return 0;
+	// }
+
+	HANDLER(dir, "List current directory")
+	{
+		FS::DirEntry entry;
+		Print("Dir: %p\n", dir);
+		VFS::DirectoryRewind(dir);
+
+		Print("Directory content:\n");
+		while(VFS::DirectoryRead(dir, &entry) == Status::Success)
+		{
+			if(!entry.isValid)
+				continue;
+
+			Print("<DATE>\t<HOUR>\t%s\t%u\t%s\n", (entry.isDirectory) ? "DIR" : "", entry.size, entry.name);
+		}
+
+		VFS::DirectoryRewind(dir);
+
+		return 0;
+	}
+
+	HANDLER(cd, "Change directory")
+	{
+		// FIXME: "cd ." causes infinite loop...
+		if(argc < 2)
+			return 1;
+
+		char* path = argv[1];
+
+		if(VFS::DirectoryChange(dir, path) != Status::Success)
+		{
+			Print("Directory \"%s\" not found!\n", path);
+			return 1;
+		}
+
+		Print("Changed to: %s\n", path);
+		return 0;
+	}
+
+	HANDLER(cat, "Read file and print contents")
+	{
+		if(argc < 2)
+			return 1;
+
+		char* path = argv[1];
+
+		FS::DirEntry entry;
+		VFS::DirectoryRewind(dir);
+
+		FS::File* file = nullptr;
+		while(VFS::DirectoryRead(dir, &entry) == Status::Success)
+		{
+			if(!entry.isValid || entry.isDirectory)
+				continue;
+
+			if(!strcmp((char*)path, (char*)entry.name))
+			{
+				VFS::FileOpen(dir, (char*)entry.name, &file);
+				break;
+			}
+		}
+
+		if(file)
+			Print("Found file: %s\n", path);
+		else
+		{
+			Print("File \"%s\" not found!\n", path);
+			return 1;
+		}
+
+		u32 readCount;
+		const u32 bufSize = 512;
+		u8 buf[bufSize];
+		Status s;
+		//bd->drv->Lock(bd->dev);
+		while((s = VFS::FileRead(file, buf, bufSize, &readCount)) == Status::Success)
+		{
+			for(unsigned a = 0; a < readCount; a++)
+			{
+				Print("%c", buf[a]);
+			}
+		}
+
+		//bd->drv->Unlock(bd->dev);
+		VFS::FileClose(&file);
+
+		return 0;
+	}
+
+	HANDLER(mkdir, "Create directory")
+	{
+		if(argc < 2)
+			return 1;
+
+		char* path = argv[1];
+
+		auto s = VFS::DirectoryCreate(dir, (char*)path);
+		Print("Status: %d - \"%s\"\n", s, path);
+	}
+
+	HANDLER(splash, "Read and display splash")
+	{
+		FS::Directory* splashDir;
+		FS::File* splashFile;
+		u32 splashRead;
+		u8* splashBuffer = new u8[320*200];
+		VFS::DirectoryOpenRoot(&splashDir);
+		VFS::DirectoryChange(splashDir, "fdd");
+		VFS::FileOpen(splashDir, "splash", &splashFile);
+		VFS::FileRead(splashFile, splashBuffer, 320*200, &splashRead);
+		Print("Splash size: %d\n", splashRead);
+
+		Print("=== VGA ===\n");
+		VGA::Init();
+		memcpy((void*)0x800a0000, splashBuffer, splashRead);
+
+		for(unsigned a = 0; a < 256; a++)
+		{
+			int v = (a << 2);
+			VGA::Write_3C8(a, v, v, v);
+		}
+	}
+
+	Container::Array<char*> SplitParameters(char* buffer, u32 bufferSize)
+	{
+		Container::Array<char*> params;
+
+		bool isWhitespace = true;
+		bool isQuoted = false;
+		for(unsigned a = 0; a < bufferSize; a++)
+		{
+			if(buffer[a] == '\0')
+				break;
+
+			if(buffer[a] == '"')
+			{
+				if(a == 0 || buffer[a - 1] != '\\')
+				{
+					isQuoted = !isQuoted;
+
+					if(!isQuoted)
+						buffer[a] = ' ';
+					else
+					{
+						isWhitespace = false;
+						params.PushBack(&buffer[a + 1]);
+						continue;
+					}
+				}
+			}
+			else if(buffer[a] == '\\' && a + 1 < bufferSize && buffer[a + 1] == '"')
+				continue;
+
+			if(isWhitespace && buffer[a] != ' ')
+			{
+				isWhitespace = false;
+				params.PushBack(&buffer[a]);
+			}
+			else if(!isWhitespace && buffer[a] == ' ')
+			{
+				if(isQuoted)
+					continue;
+
+				isWhitespace = true;
+				buffer[a] = 0;
+			}
+		}
+
+		return params;
+	}
+
 	int Thread(void* args)
 	{
-		FS::Directory* dir;
 		VFS::DirectoryOpenRoot(&dir);
 
-		char tmp[64];
-		u8 tmpX = 0;
 		Keyboard::KeyEvent keyEvent;
-		Print("=== If you need help, write \"help\" ===\n");
-		Print("> ");
+		Container::Array<char> buffer;
+		Path dirPath;
 
+		bool printPrompt = true;
 		for(;;)
 		{
-			while(Keyboard::WaitAndReadEvent(&keyEvent))
+			if(printPrompt)
+			{
+				VFS::DirectoryGetPath(dir, dirPath);
+				char tmp[Path::MaxLength];
+				dirPath.ToString(tmp);
+
+				Print("\n%s> ", tmp);
+				printPrompt = false;
+			}
+
+			if(Keyboard::WaitAndReadEvent(&keyEvent))
 			{
 				if(keyEvent.type == Keyboard::KeyType::Released)
 					continue;
 
 				if(keyEvent.key == Keyboard::KeyCode::Backspace)
 				{
-					if(tmpX > 0)
+					if(!buffer.IsEmpty())
 					{
-						tmpX--;
-						tmp[tmpX] = 0;
+						buffer.PopBack();
 						Print("\b");
 					}
 				}
 				else if(keyEvent.key == Keyboard::KeyCode::Enter)
 				{
-					tmp[tmpX] = 0;
+					if(buffer.IsEmpty())
+						continue;
 
-					if(tmpX > 0)
+					buffer.PushBack(0); // Terminate string
+					Container::Array<char*> params;
+					params = SplitParameters(&buffer[0], buffer.Size());
+
+					if(!params.IsEmpty())
 					{
-						tmpX = 0;
-						Print("\nExecuting: %s\n", tmp);
-
-						if(!strcmp(tmp, "help"))
+						for(auto func : handlers)
 						{
-							Terminal::Print("=== Available commands:               ===\n");
-							Terminal::Print("=== dir - lists current directory     ===\n");
-							Terminal::Print("=== cd <x> - enters <x> directory     ===\n");
-							Terminal::Print("=== cat <x> - reads <x> file          ===\n");
-							Terminal::Print("=== mem - prints current memory usage ===\n");
-							Terminal::Print("=== fail - kernel panic               ===\n");
-							Terminal::Print("\n");
-						}
-						else if(!strcmp(tmp, "mem"))
-						{
-							Memory::Physical::PrintMemoryMap();
-						}
-						else if(!strcmp(tmp, "fail"))
-						{
-							u8* x = (u8*)0x1234;
-							*x = 5;
-						}
-						else if(!strcmp(tmp, "task"))
-						{
-							Thread::UpdateThreadsList();
-						}
-						else if(!strcmp(tmp, "dir"))
-						{
-							//bd->drv->Lock(bd->dev);
-
-							FS::DirEntry entry;
-							Print("Dir: %p\n", dir);
-							VFS::DirectoryRewind(dir);
-
-							Print("Directory content:\n");
-							while(VFS::DirectoryRead(dir, &entry) == Status::Success)
+							if(strcmp(func.name, params[0]) == 0)
 							{
-								if(!entry.isValid)
-									continue;
+								Print("Executing: \"%s\" (%d)\n", func.name, params.Size());
+								auto res = func.handler(params.Size(), &params[0]);
+								if(res)
+									Print("Function failed with error code: %d\n", res);
 
-								Print("<DATE>\t<HOUR>\t%s\t%u\t%s\n", (entry.isDirectory) ? "DIR" : "", entry.size, entry.name);
+								break;
 							}
-
-							VFS::DirectoryRewind(dir);
-							//bd->drv->Unlock(bd->dev);
-						}
-						else if(strlen(tmp) > 3 && tmp[0] == 'c' && tmp[1] == 'd' && tmp[2] == ' ')
-						{
-							// FIXME: "cd ." causes infinite loop...
-							u8 path[256];
-							u8* dst = path;
-							bool foundAny = false;
-							for(unsigned a = 3; a < strlen(tmp); a++)
-							{
-								if(tmp[a] != ' ')
-								{
-									//(*dst++) = tolower(tmp[a]);
-									(*dst++) = tmp[a];
-									foundAny = true;
-								}
-								else if(foundAny)
-									break;
-							}
-
-							(*dst) = 0;
-
-							if(VFS::DirectoryChange(dir, (char*)path) == Status::Success)
-								Print("Changed to: %s\n", path);
-							else
-								Print("Directory \"%s\" not found!\n", path);
-						}
-						else if(strlen(tmp) > 4 && tmp[0] == 'c' && tmp[1] == 'a' && tmp[2] == 't' && tmp[3] == ' ')
-						{
-							u8 path[256];
-							u8* dst = path;
-							bool foundAny = false;
-							for(unsigned a = 4; a < strlen(tmp); a++)
-							{
-								if(tmp[a] != ' ')
-								{
-									//(*dst++) = tolower(tmp[a]);
-									(*dst++) = tmp[a];
-									foundAny = true;
-								}
-								else if(foundAny)
-									break;
-							}
-
-							(*dst) = 0;
-
-							FS::DirEntry entry;
-							VFS::DirectoryRewind(dir);
-
-							FS::File* file = nullptr;
-							while(VFS::DirectoryRead(dir, &entry) == Status::Success)
-							{
-								if(!entry.isValid || entry.isDirectory)
-									continue;
-
-								if(!strcmp((char*)path, (char*)entry.name))
-								{
-									VFS::FileOpen(dir, (char*)entry.name, &file);
-									break;
-								}
-							}
-
-							if(file)
-								Print("Found file: %s\n", path);
-							else
-							{
-								Print("File \"%s\" not found!\n", path);
-								Print("\n> ");
-								continue;
-							}
-
-							u32 readCount;
-							const u32 bufSize = 512;
-							u8 buf[bufSize];
-							Status s;
-							//bd->drv->Lock(bd->dev);
-							while((s = VFS::FileRead(file, buf, bufSize, &readCount)) == Status::Success)
-							{
-								for(unsigned a = 0; a < readCount; a++)
-								{
-									Print("%c", buf[a]);
-								}
-							}
-
-							//bd->drv->Unlock(bd->dev);
-							VFS::FileClose(&file);
-						}
-						else if(strlen(tmp) > 4 && tmp[0] == 'm' && tmp[1] == 'k' && tmp[2] == 'd' && tmp[3] == ' ')
-						{
-							u8 path[256];
-							u8* dst = path;
-							bool foundAny = false;
-							for(unsigned a = 4; a < strlen(tmp); a++)
-							{
-								if(tmp[a] != ' ')
-								{
-									//(*dst++) = tolower(tmp[a]);
-									(*dst++) = tmp[a];
-									foundAny = true;
-								}
-								else if(foundAny)
-									break;
-							}
-
-							(*dst) = 0;
-
-							auto s = VFS::DirectoryCreate(dir, (char*)path);
-							Print("Status: %d - \"%s\"\n", s, path);
-						}
-						else if(strcmp(tmp, "splash") == 0)
-						{
-							FS::Directory* splashDir;
-							FS::File* splashFile;
-							u32 splashRead;
-							u8* splashBuffer = new u8[320*200];
-							VFS::DirectoryOpenRoot(&splashDir);
-							VFS::DirectoryChange(splashDir, "fdd");
-							VFS::FileOpen(splashDir, "splash", &splashFile);
-							VFS::FileRead(splashFile, splashBuffer, 320*200, &splashRead);
-							Print("Splash size: %d\n", splashRead);
-
-							Print("=== VGA ===\n");
-							VGA::Init();
-							memcpy((void*)0x800a0000, splashBuffer, splashRead);
-
-							for(unsigned a = 0; a < 256; a++)
-							{
-								int v = (a << 2);
-								VGA::Write_3C8(a, v, v, v);
-							}
-						}
-						else
-						{
-							Print("Invalid command...\n");
 						}
 					}
 
-					Print("\n");
-
-					Path dirPath;
-					VFS::DirectoryGetPath(dir, dirPath);
-					char tmp[Path::MaxLength];
-					dirPath.ToString(tmp);
-
-					Print("%s> ", tmp);
+					printPrompt = true;
+					buffer.Clear();
 				}
 				else if(keyEvent.ascii)
 				{
-					tmp[tmpX] = keyEvent.ascii;
-					tmpX++;
+					buffer.PushBack(keyEvent.ascii);
 					Print("%c", keyEvent.ascii);
 				}
+				else
+				{
+					// INV key
+				}
 			}
-
-			//__asm("sti");
-			//__asm("hlt");
-			//Timer::Delay(100);
 		}
 	}
 }
